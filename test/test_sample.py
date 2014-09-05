6d097297322523fa44165cc47b2c665a0a96631b
Bjørn Ådlandsvik <bjorn@imr.no>
2014-09-05 08:32:40 +0200

    test for sample2D working

--- test/test_sample.py
+++ test/test_sample.py
@@ -9,19 +9,17 @@ sys.path = [".."] + sys.path
 from roppy.sample import sample2D
 
 
-class test_sample2D(unittest.TestCase):
-
-    # Mangler test for list
+class Test_sample2D(unittest.TestCase):
 
     def test_arguments(self):
         # Make a grid for testing
         imax, jmax = 10, 7
         A = np.zeros((jmax, imax))
-        
+
         # Scalars
         X, Y = 2.7, 3.5
         self.assertTrue(np.isscalar(sample2D(A, X, Y)))
-        
+
         # Conformal arrays
         X = np.array([1,2])
         Y = np.array([3,4])
@@ -40,21 +38,31 @@ class test_sample2D(unittest.TestCase):
         B = sample2D(A, X, Y)
         self.assertEqual(B.shape, (2,))
 
-        # Nonconformal arrays 
+        # Nonconformal arrays
         X = np.arange(6).reshape(2,3)
         Y = np.array([1,2])
         self.assertRaises(ValueError, sample2D, A, X, Y)
 
+        # 1D sequences such as lists
+        X = [1.1, 2.2, 3.3]
+        Y = [3.5]
+        B = sample2D(A, X, Y)
+        self.assertEqual(B.shape, (3,))
+
+    # ---------------------------------------
+
     def test_boundary(self):
+        """Works correctly close to boundary"""
         imax, jmax = 10, 7
         A = np.ones((jmax, imax))
         x, y = 0.2, 5.9
         self.assertEqual(sample2D(A, x, y), 1)
 
     def test_outside(self):
+        """Handle outside values correctly"""
         imax, jmax = 10, 7
         A = np.zeros((jmax, imax))
-        x, y = 10.2, 4
+        x, y = 9.2, 4
         self.assertRaises(ValueError, sample2D, A, x, y)
         b = sample2D(A, x, y, outside_value=np.nan)
         self.assertTrue(np.isnan(b))
@@ -80,20 +88,45 @@ class test_sample2D(unittest.TestCase):
         B = sample2D(A, i, j)
         self.assertEqual(B, A[j,i])
 
-    # Tegn opp og sjekk
+
+    # Use this landmask (origon=lower left)
+    #    | | |x|x|
+    #    |x| |x|x|
+    #    |x| | | |
+    #    | | | | |
+    #
     def test_masked(self):
-        jmax, imax = 3, 3
-        A = np.array([[0,0,1],
-                      [0,1,0],
-                      [1,1,0]])
+        """Interpolates correctly in the presence of mask"""
+        A = np.array([[1, 1, 1, 1],   # j=0
+                      [0, 1, 1, 1],   # j=1
+                      [0, 1, 0, 0],   # j=2
+                      [1, 1, 0, 0]])  # j=3
         M = A
+
         # sea point outside halo
-        x, y = 0.2, 0.2
+        x, y = 1.8, 0.8
         self.assertEqual(sample2D(A, x, y), 1)
         self.assertEqual(sample2D(A, x, y, mask=M), 1)
-        
-        
-        
-        
+
+        # sea point in halo
+        x, y = 1.8, 1.3
+        self.assertAlmostEqual(sample2D(A, x, y), 1-0.8*0.3)
+        self.assertEqual(sample2D(A, x, y, mask=M), 1)
+
+        # land point in halo
+        x, y = 1.8, 1.8
+        b_unmask = sample2D(A, x, y, undef_value=np.nan)
+        self.assertAlmostEqual(b_unmask, 1-0.8*0.8)
+        b_mask = sample2D(A, x, y, mask=M, undef_value=np.nan)
+        self.assertEqual(b_mask, 1)
+
+        # land point outside halo
+        x, y = 2.8, 2.4
+        b_unmask = sample2D(A, x, y, undef_value=np.nan)
+        self.assertEqual(b_unmask, 0)
+        b_mask = sample2D(A, x, y, mask=M, undef_value=np.nan)
+        self.assertTrue(np.isnan(b_mask))
+
+
 if __name__ == '__main__':
     unittest.main()
